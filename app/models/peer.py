e88bb380626d66696b87284f368b0e6ef7af6382
Kolawole <pro.kaiserlautern@gmail.com>
2023-12-29 04:11:13 +0100

    new changes

---
+++ app/models/peer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+import asyncio
+
+
+@dataclass
+class Peer:
+    uuid: str
+    label: str
+    avatar_url: str = None
+    public_ip: str = None
+
+    peer_state: dict = field(
+        default_factory={"id": None, "connection": None, "state": "disconnected"}
+    )
+
+    transfer_state: dict = field(
+        default_factory={
+            "file": None,
+            "info": None,
+            "sending_progress": 0,
+            "receiving_progress": 0,
+        }
+    )
+
+    state: str = "idle"
+    error_code: str = None
+
+    # Example event handling using asyncio.Event
+    state_changed_event = asyncio.Event()
+
+    async def set_state(self, new_state: str):
+        self.state = new_state
+        self.state_changed_event.set()  # Signal state change
+        if new_state != "error":
+            self.error_code = None  # Clear error code
+
+
+# Example usage
+peer = Peer(uuid="some-uuid", label="John Doe")
+
+
+async def test_():
+    await peer.set_state("has_selected_file")
---
+++ app/router.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# Configuration (similar to Ember's `config`)
+#  app.config.from_object("config.DevelopmentConfig")  # Load from a config file
+
+# Define routes
+@app.route("/")
+def index():
+    return "Welcome to the ShareDrop app!"
+
+
+@app.route("/rooms/<room_id>")  # URL parameter using angle brackets
+def room(room_id):
+    return f"You are in room {room_id}"
+
+
+if __name__ == "__main__":
+    app.run()
