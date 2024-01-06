995bf36e8b55ef94989d6b0e4f77a79b01c073e1
Kolawole <pro.kaiserlautern@gmail.com>
2024-01-06 19:42:34 +0100

    modals now working

--- app/assets/js/modal.js
+++ app/assets/js/modal.js
@@ -2,13 +2,13 @@
 const q = (selector, parent = document) => (parent || document).querySelector(selector)
 // define modals
 // about-us modal
-const about_us_modal = q('div#about-us.modal-overlay')
+const about_us_modal = q('div#about_us.modal-overlay')
 const about_us_open = q('ul > li > .icon-help')
 const about_us_close = []
 defineModal(about_us_modal, [about_us_open], about_us_close)
 
 // about-app modal
-const about_app_modal = q('div#about-app.modal-overlay')
+const about_app_modal = q('div#about_app.modal-overlay')
 const about_app_open = q('ul > li > .icon-help')
 const about_app_close = [
   q('div.actions button#close', about_app_modal)
@@ -22,8 +22,8 @@ function defineModal(modalOverlay, openTriggerElements, closeTriggerElements) {
     return
   }
   const modal = modalOverlay.querySelector('div.modal-body')
-  const closeModalOverlay = () => (modalOverlay.style.display = 'none')
-  const openModalOverlay = () => (modalOverlay.style.display = 'block')
+  const closeModalOverlay = () => modalOverlay.style.display = 'none'
+  const openModalOverlay = () => modalOverlay.style.display = 'block'
   // open event listeners
   for (const openTriggerElement of openTriggerElements) {
     openTriggerElement.addEventListener('click', function () {
@@ -34,10 +34,14 @@ function defineModal(modalOverlay, openTriggerElements, closeTriggerElements) {
   // close event listeners
   const closeButton = modal.querySelector('.close')
   if (closeButton) closeTriggerElements.push(closeButton)
-  for (const closeTriggerElement of closeTriggerElements) {
-    closeTriggerElement.addEventListener('click', function () {
-      closeModalOverlay()
-    })
+  if (closeTriggerElements.length === 0) {
+    console.warn('closeTriggerElements not found: falling back to default close trigger --> window')
+  } else {
+    for (const closeTriggerElement of closeTriggerElements) {
+      closeTriggerElement.addEventListener('click', function () {
+        closeModalOverlay()
+      })
+    }
   }
 
   // general close event listener
