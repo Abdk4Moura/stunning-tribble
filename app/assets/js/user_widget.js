1c4fb1704d218dc477eba7e443120729ef26f11b
Kolawole <pro.kaiserlautern@gmail.com>
2024-01-09 19:48:01 +0100

    webrtc added

---
+++ app/assets/js/user_widget.js
@@ -0,0 +1,22 @@
+import { Placeholder, Types } from "./genericComponents.js";
+
+const userStateNotifier = new StateNotifier(user);
+const userWidget = new NotifiedElement({
+  mountPoint: document.querySelector(".avatar"),
+  userStateNotifier,
+  templateFunction: (_) => {
+    const user = this.userStateNotifier.state
+    return {
+      'root': 'div.avatar',
+      'img': Placeholder(Types.ATTR,
+        {
+          src: user.avatar,
+          alt: user.name,
+          title: `peer id: ${user.uuid}`
+        }),
+      '.user-info .user-label #userlabel': user.name,
+    }
+  }
+});
+
+userWidget.renderComponent();
