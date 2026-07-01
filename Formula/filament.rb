ce66d87bde3a9bb5715ac0cdc863bf53261754d6
Abdk4Moura <cadaynstudio@gmail.com>
2026-07-01 13:47:58 +0000

    packaging: homebrew formula + winget manifest for cli v0.2.1-beta.27

--- Formula/filament.rb
+++ Formula/filament.rb
@@ -3,22 +3,22 @@
 class Filament < Formula
   desc "P2P file transfer between terminals and browsers - no upload, no account"
   homepage "https://filament.autumated.com"
-  version "0.2.1-beta.26"
+  version "0.2.1-beta.27"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Abdk4Moura/filament/releases/download/cli-v0.2.1-beta.26/filament-aarch64-apple-darwin.tar.gz"
-      sha256 "ffd13339ec56d890a48d928704cb1ef4e7bdb5eb964ddca9068cda32655f29a8"
+      url "https://github.com/Abdk4Moura/filament/releases/download/cli-v0.2.1-beta.27/filament-aarch64-apple-darwin.tar.gz"
+      sha256 "4544e6cb998510c8b9fba5247b4dec88e637424f36e6085988ac415fc7fdd270"
     else
-      url "https://github.com/Abdk4Moura/filament/releases/download/cli-v0.2.1-beta.26/filament-x86_64-apple-darwin.tar.gz"
-      sha256 "ed4d9e938853abee8fe9f85d39b1da6b5c2d7222a0f9783f09a7aa84a29a33e1"
+      url "https://github.com/Abdk4Moura/filament/releases/download/cli-v0.2.1-beta.27/filament-x86_64-apple-darwin.tar.gz"
+      sha256 "19d8a72e78ea8313f513b82ef812650aeee961a765a3500fc1558639c8a34bee"
     end
   end
 
   on_linux do
-    url "https://github.com/Abdk4Moura/filament/releases/download/cli-v0.2.1-beta.26/filament-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "3b8da3037784737c9c5373a9d63a0c531f1add7ff423107f20a94b710e2e012e"
+    url "https://github.com/Abdk4Moura/filament/releases/download/cli-v0.2.1-beta.27/filament-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "a24062c8f8c28c05e2705e9b5c3e44c780f8927d6ee5e5d8cc7927493ae0ffc6"
   end
 
   def install
